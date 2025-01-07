aaeefbe15ba0a884c63f6a0798684ee683c4d937
Lucas Rocha <ldrocha16@gmail.com>
2025-01-06 22:21:22 -0300

    Criação da navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection: bg-cyan-3000 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      <div
+        className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-
+      [radial-gradient(ellipse_80%_80%_at_50%_-20%,regba(120,119,198,0.3),rgba(255,
+      255,255,0))]"
+      ></div>
+      <div className="container mx-auto px-8">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+import phpEcommerce from "../assets/projects/phpEcommerce.png";
+
+export const HERO_CONTENT = `I'm an IT student and Full Stack developer, with experience in front-end technologies such as ReactJS and back-end using Java with Spring Framework, as well as databases such as MySQL, PostgreSQL and other tools. My goal is to apply and expand my knowledge, and I'm open to freelance and internship opportunities.`;
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: phpEcommerce,
+    description:
+      "A fully functional e-commerce website with features such as product listing, shopping cart and user authentication.",
+    technologies: ["HTML", "CSS", "JavaScript", "PHP", "MySQL"],
+  },
+];
+
+export const CONTACT = {
+  country: "Brazil ",
+  phoneNo: "+55 11 985353732",
+  email: "ldrocha16@gmail.com",
+};
