2f1c5ce4fd7127e28aa0fa20d98157cb61a6988a
Lucas Rocha <ldrocha16@gmail.com>
2025-01-07 07:19:52 -0300

    Criação do Header e responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Head from "./components/Head";
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection: bg-cyan-3000 selection:text-cyan-900">
+    <div className=" text-neutral-300 antialiased selection: bg-cyan-3000 selection:text-cyan-900">
       <div className="flixed top-0 -z-10 h-full w-full"></div>
       <div
         className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-
       [radial-gradient(ellipse_80%_80%_at_50%_-20%,regba(120,119,198,0.3),rgba(255,
       255,255,0))]"
-      ></div>
-      <div className="container mx-auto px-8">
-        <Navbar />
+      >
+        <div className="container mx-auto px-8">
+          <Navbar />
+          <Head />
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <p className="text-lg">LRB</p>
+        <p className="text-3xl">LRB</p>
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
