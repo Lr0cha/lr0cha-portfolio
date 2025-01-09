cee113b5a9193518bb5f7a33a217879040c4564d
Lucas Rocha <ldrocha16@gmail.com>
2025-01-08 21:40:06 -0300

    Realocação .pdf para pasta public

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,8 +21,8 @@ const Navbar = () => {
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <div className="text-base">
           <a
-            href="src/assets/resume/LucasCurriculo.pdf"
-            download="LucasCurriculo.pdf"
+            href="/resume/LucasCurriculo.pdf"
+            download
             className="border border-neutral-500 rounded p-2 hover:bg-neutral-600 text-purple-200"
           >
             <i className="fa-solid fa-download hidden sm:inline mr-2"></i>
